036da08c2455c8529e937658ae7d167400502d0c
jbooysetx <johan.booyse@gmail.com>
2025-11-19 20:14:58 +0000

    my tf code

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "example" {
-  ami           = var.my-os-image
+  ami           = "${var.my-os-image}"
   instance_type = var.machine_type
 key_name = var.private_key_name
+# adding security group to ec2 vm
+vpc_security_group_ids = [aws_security_group.jb-sec-groups.id]
   tags = {
     Name = var.vm_name
   }
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "my-vm-public-ip" {
   value = aws_instance.example.public_ip
 }
 resource "local_file" "johanip_file" {
-  content = aws_instance.example.public_ip
+  content = "My IP : ${aws_instance.example.public_ip}"
   filename = "${path.module}/johanip.txt"
 }
